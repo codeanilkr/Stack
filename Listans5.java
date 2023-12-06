adb785a62c9ee77a7968301554c8f61347fab547
Anil Kumar <mysteriousanil@gmail.com>
2023-12-06 12:46:43 +0530

    Take elements(numbers in the range of 1-50) of a Linked List as input from the user. Delete all nodes which have values greater than 25.

---
+++ Listans5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Listans5 {
+    
+    public static void main(String[] args){
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        System.out.println("Enter Value 1 to 50");
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<=10; i++){
+            list.add(i, sc.nextInt());
+        }
+        System.out.println(list);
+
+ListIterator<Integer> ll = list.listIterator();
+while(ll.hasNext()){
+    if(ll.next()>25){
+        ll.remove();
+    }
+}
+System.out.println(list);
+
+
+    }
+}
